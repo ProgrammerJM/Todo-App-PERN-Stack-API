bd9c1f605c4ba33ffb17740d800e8032ab04667d
John Mark Tizado <johnmarktizado@gmail.com>
2024-07-30 01:57:39 +0800

    chore: Update database configuration and add Vercel deployment configuration file

--- db.js
+++ db.js
@@ -1,12 +1,20 @@
 const Pool = require("pg").Pool;
-require("dotenv").config();
+// require("dotenv").config();
 
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.POSTGRES_URL,
+});
+
+pool.connect((err, client, release) => {
+  if (err) throw err;
+  console.log("Connet to PostgresSQL successfully");
 });
 
 module.exports = pool;
