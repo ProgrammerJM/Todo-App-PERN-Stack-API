e5c6df6a36af901efdef14dcd1b2c7dadff22264
John Mark Tizado <johnmarktizado@gmail.com>
2024-07-30 15:45:19 +0800

    refactor: Update database configuration to use environment variables

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.HOST,
+  host: process.env.HOSTNAME,
   port: process.env.PORT,
-  user: process.env.USER,
+  user: process.env.USERNAME,
   password: process.env.PASSWORD,
-  database: process.env.NAME,
+  database: process.env.DATABASENAME,
 });
 
 module.exports = pool;
